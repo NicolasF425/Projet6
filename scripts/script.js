268af2e552680fb71eb2b73cb2185b6166868e44
NicolasF425 <nifan42@gmail.com>
2025-02-06 11:41:56 +0100

    fusion scripts et mag menu deroulant projet6

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,169 @@
+// VARIABLES
+let base_url = "http://localhost:8000/api/v1/titles/";
+let liste_genres = [];
+let genres_fixes = ["Mystery", "Drama"];
+
+
+async function getMeilleursFilms() {
+  let url_best_imdb_scores = base_url + "?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=";
+  let liste_meilleurs_films = []
+  next =""
+
+  // récupération des films par scores décroissants
+  try {
+    // les 5 premiers
+    response = await fetch(url_best_imdb_scores);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    json_scores = await response.json();
+    for (let i=1; i<5; i++) {
+      liste_meilleurs_films.push(json_scores.results[i])
+    }
+    // les 5 suivants
+    next_url = json_scores.next
+    response = await fetch(next_url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    json_scores_next = await response.json();
+    for (let i=0; i<2; i++) {
+      liste_meilleurs_films.push(json_scores_next.results[i])
+    }
+
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  // on sélectionne le premier de la liste
+  try {
+      response = await fetch(base_url + json_scores.results[0].id);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      json_best = await response.json();
+      //console.log(json_best);
+    } catch (error) {
+      console.error(error.message);
+    }
+  
+  imageUrl = json_best.image_url;
+  img = document.createElement('img');
+  img.src = imageUrl;
+  img.alt = json_best.title;
+  const titre = document.createElement('p')
+  titre.textContent = json_best.title;
+  const desc = document.createElement('p');
+  desc.textContent = json_best.description;
+  let button = document.createElement('button')
+  button.textContent = "Détails";
+  button.id = json_best.id;
+  button.addEventListener("click", function (e) {});
+
+  let modal = document.getElementById("fiche_film");
+  let span = document.getElementsByClassName("close")[0];
+  // cliquer sur le bouton affiche la fenetre
+  button.onclick = function() {
+    modal.style.display = "block";
+    getFilmInfos(button.id);
+  }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  // Ajout des éléments
+  const img_container = document.getElementById('meilleur-film');
+  img_container.innerHTML = ''; // vide le conteneur
+  img_container.appendChild(img);
+  let div = document.createElement('div');
+  div.appendChild(titre);
+  div.appendChild(desc);
+  div.appendChild(button);
+  img_container.appendChild(div);
+  img_container.classList.add("cadre-noir");
+  
+  // on sélectionne les 6 suivants
+  const img_containers = document.getElementById('meilleurs-films');
+  for (let i=0; i<6; i++){
+    try {
+      response = await fetch(base_url + liste_meilleurs_films[i].id);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      json = await response.json();
+      // Ajout des éléments
+      imageUrl = json.image_url;
+      img = document.createElement('img');
+      img.src = imageUrl;
+      img.alt = json.title;
+      let text = document.createElement('p');
+      text.textContent = json.title;
+      let button = document.createElement('button')
+      button.textContent = "Détails";
+      button.id = json.id;
+      button.addEventListener("click", function (e) {});
+
+      let modal = document.getElementById("fiche_film");
+      let span = document.getElementsByClassName("close")[0];
+      // cliquer sur le bouton affiche la fenetre
+      button.onclick = function() {
+        modal.style.display = "block";
+        getFilmInfos(button.id);
+      }
+      // un click sur le <span> (x) la cache
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+
+      let div = document.createElement('div');
+      div.appendChild(img);
+      div.appendChild(text);
+      div.appendChild(button);
+      if (i<2) {
+        div.classList.add("display-1-2");
+      }
+      if (i>1 && i<4) {
+        div.classList.add("display-3-4");
+      }
+      if(i>3) {
+        div.classList.add("display-5-6");
+      }
+      img_containers.appendChild(div);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  let button_plus = document.getElementById("plus_best");
+  button_plus.addEventListener("click", function (e) {});
+  button_plus.onclick = function() {
+    let meilleurs_films = document.getElementById('meilleurs-films');
+    if (button_plus.textContent == "Voir plus") {
+      let divs34 = meilleurs_films.getElementsByClassName("display-3-4");
+      if (divs34.length > 0) {
+        divs34.item(0).classList.add("display-1-2");
+        divs34.item(1).classList.add("display-1-2");  
+        divs34.item(0).classList.remove("display-3-4");
+        divs34.item(0).classList.remove("display-3-4");
+      }
+      let divs56 = meilleurs_films.getElementsByClassName("display-5-6");
+      if (divs56.length > 0) {
+        divs56.item(0).classList.add("display-1-2");
+        divs56.item(1).classList.add("display-1-2");  
+        divs56.item(0).classList.remove("display-5-6");
+        divs56.item(0).classList.remove("display-5-6");
+      }
+      button_plus.textContent = "Voir moins";
+    }
+  }
+}
+
+
 async function getFilmsGenre(genre_choisi) {
-  let base_url = "http://localhost:8000/api/v1/titles/";
-  let genres = ["Mystery", "Drama", genre_choisi];
+  let genres = [genres_fixes[0], genres_fixes[1], genre_choisi];
   let ids = ["genre1", "genre2", "genre_choisi"]
   let url_genre = ""; 
   let liste_films = []
-
-  let tous_genres = await getListeGenres();
 
   for (let boucle=0; boucle<3; boucle++) {
     liste_films = []
@@ -95,15 +253,14 @@ async function getFilmsGenre(genre_choisi) {
         console.error(error.message);
       }
     }
-
   }
 
   // menu déroulant
   let menu = document.getElementById('menu-choix-genre');
-  for (let g=0; g<tous_genres.length; g++){
+  for (let g=0; g<liste_genres.length; g++){
     let item = document.createElement('option');
-    item.value = tous_genres[g];
-    item.textContent = tous_genres[g];
+    item.value = liste_genres[g];
+    item.textContent = liste_genres[g];
     menu.appendChild(item);
   }
   document.getElementById('menu-choix-genre').addEventListener('change', function() {
@@ -112,10 +269,11 @@ async function getFilmsGenre(genre_choisi) {
   });
 }
 
+
 async function getFilmsGenreChoisi(genre_choisi) {
   let base_url = "http://localhost:8000/api/v1/titles/";
   liste_films = []
-  url_genre = base_url + "?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre="+genre_choisi+"&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=";
+  url_genre = base_url + "?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre="+genre_choisi+"&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=";
   // récupération des films par scores décroissants
   try {
     // les 5 premiers
@@ -184,6 +342,7 @@ async function getFilmsGenreChoisi(genre_choisi) {
   }
 }
 
+
 async function getFilmInfos(id) {
   let url = "http://localhost:8000/api/v1/titles/"+id;
   response = await fetch(url);
@@ -203,13 +362,13 @@ async function getFilmInfos(id) {
   }
 }
 
+
 async function getListeGenres() {
   let urls = ["http://localhost:8000/api/v1/genres", "http://localhost:8000/api/v1/genres/?page=2",
     "http://localhost:8000/api/v1/genres/?page=3", "http://localhost:8000/api/v1/genres/?page=4",
     "http://localhost:8000/api/v1/genres/?page=5"
   ];
-  let liste_genres = [];
-
+  
   for (let i=0; i<urls.length; i++){
     response = await fetch(urls[i]);
     try {
@@ -218,16 +377,18 @@ async function getListeGenres() {
       }
       json_genres = await response.json();
       for (let j=0; j<5; j++){
+        if (json_genres.results[j].name != genres_fixes[0] && json_genres.results[j].name != genres_fixes[1])
         liste_genres.push(json_genres.results[j].name);
       }
     } catch (error) {
       console.error(error.message);
     }
   }
-  return liste_genres;
 }
 
 
+getListeGenres();
+getMeilleursFilms();
 getFilmsGenre("Horror");
 
 
